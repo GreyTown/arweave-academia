fccad219ab0d3542f95a7f52e4c4613c81908d32
jamiebones <jamiebones2000@yahoo.co.uk>
2023-07-29 19:34:27 +0100

    did cleanup and installed dependencies

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,15 @@
 import type { AppProps } from 'next/app';
-import Web3Provider from '@/providers/Web3';
+import { ArweaveWalletKit } from "arweave-wallet-kit";
 import { SEO } from '@/components/layout';
+
 
 import '@/styles/globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
-		<Web3Provider>
+		<ArweaveWalletKit>
 			<SEO />
 			<Component {...pageProps} />
-		</Web3Provider>
+		</ArweaveWalletKit>
 	);
 }
